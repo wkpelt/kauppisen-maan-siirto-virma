64efa0caf5ea8c0fcc878205491a9725384aa51e
Matias Lappalainen <matias.lappalainen@tieto.com>
2019-02-13 19:27:17 +0200

    Tyhjennetty app.js

--- app/src/App.js
+++ app/src/App.js
@@ -6,8 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
-        <h1>Hello</h1>
+        <h1>Wunder workshop</h1>
       </div>
     );
   }
