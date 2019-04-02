95ab7b58f66a784a2c7ffcaad1443dd67a379697
wkpelt <wkpelt@utu.fi>
2019-04-02 22:12:02 +0300

    fetch working now properly

--- frontend/src/Components/Ajoneuvo/Ajoneuvo.js
+++ frontend/src/Components/Ajoneuvo/Ajoneuvo.js
@@ -7,22 +7,20 @@ import Card from '../../Components/Card/Card';
 class Ajoneuvo extends Component {
     constructor(props) {
         super(props);
-    
         this.state = {
           imgsrc: this.props.url,
         };
       }
     
     render(){
-        const urli = this.props.url
+        //const urli = this.props.url
 
-        const joo = urli.substr(20)
+        //const joo = urli.substr(20)
         return(
-            
+
         <Card>
         <p>Ajoneuvo</p>
-        
-        <img className="image"src=""/>
+        <img className="image"src={this.props.url}/>
         </Card>
         )
     }
